ec1ea12e9d300d66e338e2b417a062efa223b73f
Ayanbola <olaoluwaayanbola@gmail.com>
2023-03-08 21:29:33 +0100

    finished authentication

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,47 +1,54 @@
 const express = require('express');
 const Router = express.Router();
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/auth');
 /**
  * register new users
  */
-Router.post('/signup', async (req, res) => {
-    const saltRounds = 10
+Router.post('/signup', async (req, res, next) => {
+    const saltRounds = 10;
     try {
         const { username, password } = req.body;
         /**
          * hash the users password
          */
+        // Validate input
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Please provide both username and password' });
+        }
         bcrypt.hash(password, saltRounds, async (err, hash) => {
-            try{
-                console.log('Hash: ', hash)
+            try {
+                console.log('Hash: ', hash);
                 const newUser = new User({
                     username: username,
                     password: hash,
                 });
                 const savedUser = await newUser.save();
-                res.status(201).json({ user: savedUser });
-                const token = jwt.sign({ userId: savedUser._id }, process.env.JWT_SECRET);
-            }catch(err) {
-                console.log(err)
+                // const token = jwt.sign({ userId: savedUser._id }, process.env.JWT_SECRET);
+                res.status(201).json({ savedUser });
+            } catch (err) {
+                console.log(err);
             }
         });
-
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        next(err);
     }
 });
 
 /**
  * Login users
  */
-Router.get('/login', async (req, res) => {
+Router.post('/login', async (req, res, next) => {
     try {
         const { username, password } = req.body;
         /**
          * get the data from the server using the current username
          */
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Please provide both username and password' });
+        }
+
         const userDb = await User.findOne({ username });
         if (!userDb) {
             return res.status(401).json({ message: 'Invalid credentials' });
@@ -49,10 +56,25 @@ Router.get('/login', async (req, res) => {
         /**
          * authentication Logic
          */
+        const validateUser = async (hash) => {
+            bcrypt
+                .compare(password, hash)
+                .then(async (res) => {
+                    console.log(res)
+                })
+                .catch((err) => console.error(err.message));
+        };
+        validateUser(userDb.password);
+        res.status(200).send({ userDb });
     } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: err.message });
+        next(err);
     }
+});
+
+Router.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err });
+    next();
 });
 
 module.exports = Router;
