44510046efcc131ba1b6ace9164b2488bc0d4385
Ayanbola <olaoluwaayanbola@gmail.com>
2023-03-13 19:40:44 +0100

    authorization update

--- src/models/post.js
+++ src/models/post.js
@@ -1,26 +1,30 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  user: {
-    type: String,
-    required: true,
-  },
+   title: {
+      type: String,
+      required: true,
+   },
+   content: {
+      type: String,
+      required: true,
+   },
+   token: {
+      type: String,
+      require: false,
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now,
+   },
+   updatedAt: {
+      type: Date,
+      default: Date.now,
+   },
+   user: {
+      type: String,
+      required: true,
+   },
 });
 
 const Post = mongoose.model('Post', postSchema);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const post = require('../controllers/post');
-const auth = require('../services/auth');
-const user = require("../controllers/users")
+const auth = require('../services/authentication');
+const user = require('../controllers/users');
 
 app.use('/auth', auth);
 app.use('/users', post);
